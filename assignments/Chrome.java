568514928487db105a053512dd58bed2b42a0e2f
Janapriya105 <janapriyars6@gmail.com>
2025-11-09 15:29:18 +0530

    Add files via upload
    Assignments

---
+++ assignments/Chrome.java
@@ -0,0 +1,20 @@
+package assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+        String developer = "Google";
+        boolean isBeta = false;
+        int releaseYear = 2008;
+        char shortcutKey = 'C';
+
+        // Print the values
+        System.out.println("Chrome Version: " + version);
+        System.out.println("Developer: " + developer);
+        System.out.println("Is Beta Version: " + isBeta);
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Shortcut Key: " + shortcutKey);
+}
+
+}
---
+++ assignments/FindOddNumbers.java
@@ -0,0 +1,24 @@
+package assignments;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+	System.out.println("The odd numbers between 1 to 10 ");
+	int maxRange = 10;
+	
+	for (int i = 1; i <= maxRange; i++) {
+
+		if (i % 2 == 1) {
+			
+			System.out.println(i); 
+		}
+
+		
+	}
+
+	
+	
+	
+	
+}
+}
